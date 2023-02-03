ac4a6e2fbf4ef70739cc683a2790ccc236d2cc65
saurabhnimkande <saurabhnimkande@gmail.com>
2023-02-03 09:48:36 +0530

    strict array check implementation and license added

--- src/index.js
+++ src/index.js
@@ -1,29 +1,81 @@
 import { arrayCompare } from "./array/array.js";
+import { strictArrayCompare } from "./array/strictArrayCheck.js";
 import { objectCompare } from "./object/object.js";
+import { strictObjectCompare } from "./object/strictObjectCheck.js";
 import { isObject } from "./utils/index.js";
 
-export const deepCompare = (one, two) => {
+export const deepCompare = (
+  one,
+  two,
+  strictCheck = false,
+  entityOneName,
+  entityTwoName
+) => {
   try {
-    if (Array.isArray(one) && Array.isArray(two)) {
-      return arrayCompare(one, two, "Array1", "Array2");
-    } else if (
-      isObject(one) &&
-      isObject(two) &&
-      !Array.isArray(one) &&
-      !Array.isArray(two)
-    ) {
-      return objectCompare(one, two, "Object1", "Object2");
+    if (strictCheck) {
+      if (Array.isArray(one) && Array.isArray(two)) {
+        return strictArrayCompare(
+          one,
+          two,
+          entityOneName || "Array1",
+          entityTwoName || "Array2"
+        );
+      } else if (
+        isObject(one) &&
+        isObject(two) &&
+        !Array.isArray(one) &&
+        !Array.isArray(two)
+      ) {
+        return strictObjectCompare(
+          one,
+          two,
+          entityOneName || "Object1",
+          entityTwoName || "Object2"
+        );
+      } else {
+        return {
+          status: false,
+          result: {
+            pathOne: `DataType1`,
+            valueOne: one,
+            pathTwo: `Datatype2`,
+            valueTwo: two,
+            reason: `Please provide same datatypes to compare`,
+          },
+        };
+      }
     } else {
-      return {
-        status: false,
-        result: {
-          pathOne: `DataType1`,
-          valueOne: one,
-          pathTwo: `Datatype2`,
-          valueTwo: two,
-          reason: `Please provide same datatypes to compare`,
-        },
-      };
+      if (Array.isArray(one) && Array.isArray(two)) {
+        return arrayCompare(
+          one,
+          two,
+          entityOneName || "Array1",
+          entityTwoName || "Array2"
+        );
+      } else if (
+        isObject(one) &&
+        isObject(two) &&
+        !Array.isArray(one) &&
+        !Array.isArray(two)
+      ) {
+        return objectCompare(
+          one,
+          two,
+          entityOneName || "Object1",
+          entityTwoName || "Object2"
+        );
+      } else {
+        return {
+          status: false,
+          result: {
+            pathOne: `DataType1`,
+            valueOne: one,
+            pathTwo: `Datatype2`,
+            valueTwo: two,
+            reason: `Please provide same datatypes to compare`,
+          },
+        };
+      }
     }
   } catch (e) {
     return e;
